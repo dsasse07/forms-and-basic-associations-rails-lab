2707f54ed4d8e35d0c5da26821e1bddf9af024b8
Daniel Sasse <dsasse07@gmail.com>
2021-01-14 13:33:42 -0500

    commit tree 693c439c3e7c63156c5e60b8af579340865d08a6

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,35 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(artist)
+    self.artist = Artist.find_or_create_by(name: artist)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(id: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      next if content.length == 0
+        new_note = Note.find_or_create_by(content: content)
+        self.notes << new_note
+    end    
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
+
 end
